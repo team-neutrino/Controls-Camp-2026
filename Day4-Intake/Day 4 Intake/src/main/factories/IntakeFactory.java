4dc0ee25e0bc80b08986b39d844ecfab4a21f0a7
neutrino <neutrino3928@outlook.com>
2026-06-16 19:31:34 -0500

    WIP

--- Day4-Intake/Day 4 Intake/src/main/factories/IntakeFactory.java
+++ Day4-Intake/Day 4 Intake/src/main/factories/IntakeFactory.java
@@ -1,5 +1,11 @@
 package frc.robot.factories;
 
 public class IntakeFactory {
+    public static Command runIntake() {
+        return intake.runIntake(INTAKE_VOLTAGE);
+    }
 
+    public static Command runOuttake() {
+        return intake.runIntake(OUTTAKE_VOLTAGE);
+    }
 }
--- Day4-Intake/Day 4 Intake/src/main/java/frc/robot/subsystems/Intake.java
+++ Day4-Intake/Day 4 Intake/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,11 +1,91 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RioConstants;
+import static frc.robot.Constants.IntakeConstants.*;
 
 public class Intake extends SubsystemBase {
+  private final CANBus m_CANbus = RioConstants.RIO_BUS;
+  private TalonFX m_rollerMotor = new TalonFX(ROLLER_MOTOR_ID, m_CANbus);
+  private TalonFX m_rollerFollowerMotor = new TalonFX(FOLLOWER_MOTOR_ID, m_CANbus);
+  private TalonFX m_deployMotor = new TalonFX(DEPLOY_MOTOR_ID, m_CANbus);
+
+  private TalonFXConfiguration m_rollerMotorConfig = new TalonFXConfiguration();
+  private TalonFXConfiguration m_deployMotorConfig = new TalonFXConfiguration();
+  private final CurrentLimitsConfigs m_rollerCurrentLimitConfig = new CurrentLimitsConfigs();
+  private final CurrentLimitsConfigs m_deployCurrentLimitConfig = new CurrentLimitsConfigs();
+
+  private boolean m_isDeployed = false;
+  private double m_targetAngle;
+  private double m_rollerMotorVoltage;
+  private final PositionVoltage m_deployPositionControl = new PositionVoltage(0);
+  private final VoltageOut m_rollerVoltageControl = new VoltageOut(0);
 
   public Intake() {
+    m_deployCurrentLimitConfig.withSupplyCurrentLimit(DEPLOY_CURRENT_LIMIT)
+        .withSupplyCurrentLimitEnable(true)
+        .withStatorCurrentLimit(DEPLOY_CURRENT_LIMIT)
+        .withStatorCurrentLimitEnable(true);
+    m_rollerCurrentLimitConfig.withSupplyCurrentLimit(ROLLER_CURRENT_LIMIT)
+        .withSupplyCurrentLimitEnable(true)
+        .withStatorCurrentLimit(ROLLER_CURRENT_LIMIT)
+        .withStatorCurrentLimitEnable(true);
+    m_deployMotorConfig.CurrentLimits = m_deployCurrentLimitConfig;
+    m_rollerMotorConfig.CurrentLimits = m_rollerCurrentLimitConfig;
+
+    m_deployMotorConfig.Slot0.kP = INTAKE_kP;
+    m_deployMotorConfig.Slot0.kI = INTAKE_kI;
+    m_deployMotorConfig.Slot0.kD = INTAKE_kD;
+
+    m_rollerMotor.getConfigurator().apply(m_rollerMotorConfig);
+    m_rollerFollowerMotor.getConfigurator().apply(m_rollerMotorConfig);
+    m_deployMotor.getConfigurator().apply(m_deployMotorConfig);
+    m_rollerMotor.setNeutralMode(NeutralModeValue.Coast);
+    m_rollerFollowerMotor.setNeutralMode(NeutralModeValue.Coast);
+    m_deployMotor.setNeutralMode(NeutralModeValue.Coast);
+    m_deployMotor.setPosition(0);
+    m_rollerMotorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+    Follower followRequest = new Follower(ROLLER_MOTOR_ID, MotorAlignmentValue.Opposed);
+    m_rollerFollowerMotor.setControl(followRequest);
+  }
+
+  public double getMotorAngle() {
+    return m_deployMotor.getPosition().getValueAsDouble();
+  }
+
+  public double getTargetAngle() {
+    return m_targetAngle;
+  }
+
+  public double getRollerRPM() {
+    return m_rollerMotor.getVelocity().getValueAsDouble() * 60;
+  }
+
+  private void moveToIntake(double targetPosition) {
+    m_deployMotor.setControl(m_deployPositionControl.withPosition(targetPosition));
+  }
+
+  private void spinRoller(double voltage) {
+    m_rollerMotor.setControl(m_rollerVoltageControl.withOutput(voltage));
+  }
+
+  @Override
+  public void periodic() {
+    spinRoller(m_rollerMotorVoltage);
+    moveToIntake(m_targetAngle);
   }
 
   /**
@@ -20,13 +100,29 @@ public class Intake extends SubsystemBase {
         });
   }
 
-  public boolean exampleCondition() {
-    return false;
+  public Command runIntake(double speed) {
+    return run(() -> {
+      m_rollerMotorVoltage = speed;
+    });
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public Command toggleIntake() {
+    return runOnce(() -> {
+      if (m_isDeployed) {
+        m_targetAngle = DEPLOYED_POSITION;
+      }
+      m_targetAngle = 0;
+    });
+  }
+
+  public Command deployIntake() {
+    return runOnce(() -> {
+
+    });
+  }
+
+  public boolean exampleCondition() {
+    return false;
   }
 
   @Override
