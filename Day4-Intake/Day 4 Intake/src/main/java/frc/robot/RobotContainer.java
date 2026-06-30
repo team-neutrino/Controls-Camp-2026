3a02656be4a1b8495d62b30b7755e4d57929d041
Allen Cai <152452752+allencai11@users.noreply.github.com>
2026-06-30 11:17:06 -0500

    Edit command bindings to be accurate

--- Day4-Intake/Day 4 Intake/src/main/java/frc/robot/RobotContainer.java
+++ Day4-Intake/Day 4 Intake/src/main/java/frc/robot/RobotContainer.java
@@ -23,8 +23,8 @@ public class RobotContainer {
   private void configureBindings() {
     m_driverController.leftBumper().whileTrue(IntakeFactory.runIntakeCommand());
     m_driverController.rightBumper().whileTrue(IntakeFactory.runOuttake());
-    m_driverController.x().onTrue(IntakeFactory.deployIntakeCommand());
-    m_driverController.y().onTrue(IntakeFactory.retractIntakeCommand());
+    m_driverController.leftTrigger().onTrue(IntakeFactory.deployIntakeCommand());
+    m_driverController.rightTrigger().onTrue(IntakeFactory.retractIntakeCommand());
     m_driverController.b().onTrue(IntakeFactory.toggleIntakeCommand());
   }
 
