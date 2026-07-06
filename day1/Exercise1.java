407305db1711f00afc33693ebd330cf44a6c2b8a
Neutrino Controls <controlsneutrino@gmail.com>
2026-07-06 16:46:20 -0500

    !! use this branch
    use this branch to set up the computers and not the answer key

--- day1/Exercise1.java
+++
@@ -1,8 +0,0 @@
-package day1;
-
-public class Exercise1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("You're wonderful");
-    }
-}
--- day1/Exercise3.java
+++
@@ -1,9 +0,0 @@
-package day1;
-
-public class Exercise3 {
-    public static void main(String[] args) {
-        boolean variable = true;
-
-        System.out.println("THIS STUPID VARIABLE IS " + variable);
-    }
-}
