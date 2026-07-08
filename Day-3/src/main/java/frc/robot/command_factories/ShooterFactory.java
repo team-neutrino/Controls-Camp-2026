c5977ad80cc370ef4b7f025e1a5a075700bd74b9
Neutrino <neutrino3928@outlook.com>
2026-07-08 17:48:01 -0500

    Hopefully good for now

--- Day-3/src/main/java/frc/robot/command_factories/ShooterFactory.java
+++ Day-3/src/main/java/frc/robot/command_factories/ShooterFactory.java
@@ -3,16 +3,17 @@ package frc.robot.command_factories;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.Shooter;
+import static frc.robot.SubsystemContainer.*;
 
 public class ShooterFactory {
-    private final Shooter shooter;
+    private final Shooter m_shooter;
 
     public ShooterFactory(Shooter subsystem) {
-        shooter = subsystem;
+        m_shooter = subsystem;
     }
 
     public Command runMotor() {
-        return shooter.runMotor();
+        return m_shooter.runMotor();
     }
 
     // public Command runMotorSlow() {
@@ -20,6 +21,6 @@ public class ShooterFactory {
     // }
 
     public Command doNothing() {
-        return shooter.doNothing();
+        return m_shooter.doNothing();
     }
 }
